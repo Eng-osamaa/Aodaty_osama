812d362784f639f618d5a824068ab0d5d3e5914a
Osama Al-Badany <aalbdany054@gmail.com>
2026-07-30 19:36:35 +0300

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Aodaty_osama",
+  assetPrefix: "/Aodaty_osama/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
